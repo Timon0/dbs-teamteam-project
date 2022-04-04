a1191b7bee1bf6b07a3ffc2e323a9979566e75c0
Timon Kurmann <timon.kurmann@gmail.com>
2022-04-04 15:55:03 +0200

    add interactive analytics

--- sql/create view.sql
+++ sql/create view.sql
@@ -12,3 +12,19 @@ select canton.sbbregion_isocode,
     where weather.date >= '2021-01-01'
     group by canton.sbbregion_isocode, weather.date
     order by weather.date;
+    
+create view weatherdailysnowdelay (sbbregion_isocode, date, rainfall, snowdepth, temp, zugpuenktlichkeit)
+as
+select canton.sbbregion_isocode,
+		weather.date,
+        avg(weather.rainfall) as rainfall,
+		avg(weather.snowdepth) as snowdepth,
+        avg(weather.airtemp_mean) as temp,
+        delay.zugpuenktlichkeit 
+	from weatherdailymeasurement as weather
+	inner join weatherstation as station on weather.weatherstation_isocode = station.isocode
+    inner join canton as canton on canton.isocode = station.canton_isocode
+    inner join sbbdelay as delay on delay.date = weather.date and delay.sbbregion_isocode = canton.sbbregion_isocode
+    where weather.date >= '2021-01-01'
+    group by canton.sbbregion_isocode, weather.date
+    order by weather.date;    
