5cdb4661b77b5b59881c1c87488e5cd569b18617
Gr0unding <73466358+Gr0unding@users.noreply.github.com>
2022-04-12 19:44:19 +0200

    Added view "GroupedBySnowfall".

--- sql/create view.sql
+++ sql/create view.sql
@@ -27,4 +27,21 @@ select canton.sbbregion_isocode,
     inner join sbbdelay as delay on delay.date = weather.date and delay.sbbregion_isocode = canton.sbbregion_isocode
     where weather.date >= '2021-01-01'
     group by canton.sbbregion_isocode, weather.date
-    order by weather.date;    
+    order by weather.date;  
+    
+        
+create view weatherdailydelay_groupedbysnowfall (sbbregion_isocode, date, rainfall, temp, zugpuenktlichkeit, range_step)
+as
+select canton.sbbregion_isocode,
+		weather.date,
+        avg(weather.rainfall) as rainfall,
+        avg(weather.airtemp_mean) as temp,
+        delay.zugpuenktlichkeit,
+        (select max(subselect.rainfall) from weatherdailydelay as subselect where subselect.sbbregion_isocode = canton.sbbregion_isocode and temp < 0) / 5 as range_step
+	from weatherdailymeasurement as weather
+	inner join weatherstation as station on weather.weatherstation_isocode = station.isocode
+    inner join canton as canton on canton.isocode = station.canton_isocode
+    inner join sbbdelay as delay on delay.date = weather.date and delay.sbbregion_isocode = canton.sbbregion_isocode
+    where weather.date >= '2021-01-01'
+    group by canton.sbbregion_isocode, weather.date
+    order by weather.date;
