4a2098493bc667a91df8018931b927e261d9f750
michaeljakober <michael_jakober@bluewin.ch>
2022-04-26 17:20:41 +0200

    Add new views and new horizontal grouped bar-charts

--- sql/create view.sql
+++ sql/create view.sql
@@ -56,3 +56,30 @@ select 	sbbregion_isocode,
 	where temp < 0
     group by sbbregion_isocode, `range`
     order by sbbregion_isocode, `range`;
+
+create view weatherdailydelay_groupedbyrainfall (sbbregion_isocode, date, rainfall, temp, zugpuenktlichkeit, range_step)
+as
+select canton.sbbregion_isocode,
+		weather.date,
+        avg(weather.rainfall) as rainfall,
+        avg(weather.airtemp_mean) as temp,
+        delay.zugpuenktlichkeit,
+        (select max(subselect.rainfall) from weatherdailydelay as subselect where subselect.sbbregion_isocode = canton.sbbregion_isocode and temp > 0) / 4 as range_step
+	from weatherdailymeasurement as weather
+	inner join weatherstation as station on weather.weatherstation_isocode = station.isocode
+    inner join canton as canton on canton.isocode = station.canton_isocode
+    inner join sbbdelay as delay on delay.date = weather.date and delay.sbbregion_isocode = canton.sbbregion_isocode
+    where weather.date >= '2021-01-01'
+    group by canton.sbbregion_isocode, weather.date
+    order by weather.date;
+
+create view weatherdailydelay_rainfallinranges (sbbregion_isocode, avg_delay, `range`, range_step)
+as
+select 	sbbregion_isocode,
+		avg(100 - zugpuenktlichkeit) as avg_delay,
+        concat(range_step * floor(rainfall / range_step), ' mm - ', range_step * floor(rainfall / range_step) + range_step, ' mm') as `range`,
+        range_step
+	from weatherdailydelay_groupedbysnowfall as weather
+	where temp > 0
+    group by sbbregion_isocode, `range`
+    order by sbbregion_isocode, `range`;
