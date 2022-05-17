0d4bb92f6c56c7fb943946c4b4aece33c83c6f4b
Gr0unding <73466358+Gr0unding@users.noreply.github.com>
2022-05-17 20:38:32 +0200

    Zusätzlicher Index für weatherstation Tabelle

--- sql/performance.sql
+++ sql/performance.sql
@@ -1,3 +1,5 @@
 create index date_index on weatherdailymeasurement(date);
 
 create index date_sbbregion_index on sbbdelay(sbbregion_isocode, date);
+
+create index weatherstation_canton_isocode_index on weatherstation(canton_isocode, isocode);
